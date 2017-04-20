58b5a368c4dbe05c9a9bf5078ec3d128c08189ea
Ryan Cronin <Ryan@Ryans-MBP.lan>
2017-04-20 13:59:38 -0400

    added function for getParkByID

--- src/frontend/js/park.service.js
+++ src/frontend/js/park.service.js
@@ -8,22 +8,37 @@
 
   function ParksService($http, ParksService){
 
-    function getAllParks(){
-    return $http({
-      url: 'https://findthebark.herokuapp.com/dog-parks',
-      method: 'GET'
+    function getAllParks() {
+      return $http({
+        url: 'https://findthebark.herokuapp.com/dog-parks',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
       })
       .then(function handleResponse(response){
         return response.data;
       });
+      // add catch
     }
 
-
+    function getParkById(id) {
+      return $http({
+        url: 'https://findthebark.herokuapp.com/dog-parks' + id,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(function handleResponse(response) {
+        return response.data;
+      });
+      // add catch
+    }
 
     return {
-      getAllParks: getAllParks
+      getAllParks: getAllParks,
+      getParkById: getParkById
     };
-
   }
-
 }());
