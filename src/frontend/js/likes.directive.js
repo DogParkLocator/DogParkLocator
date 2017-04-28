59ac5986ea2e8ab0dce449e52302d9f3bc200c72
Brian Fiala <bgfiala@gmail.com>
2017-04-28 15:09:18 -0400

    likes directive for park lists complete

--- src/frontend/js/likes.directive.js
+++ src/frontend/js/likes.directive.js
@@ -6,39 +6,36 @@
 
   Likes.$inject = ['ParksService'];
   let $ = angular.element;
-  let vm = this;
 
-  function Likes() {
+  function Likes(ParksService) {
     return{
       templateUrl: 'views/park-likes.template.html',
       restrict: 'E',
-      link: updateLikes,
+      link: updateLikeDislike,
       scope: {
         park: '='
       }
     };
 
-    function updateLikes(scope, element) {
-      $(element.querySelector('.glyphicon-thumbs-up'))
+    function updateLikeDislike(scope, element) {
+      $(element[0].querySelector('.glyphicon-thumbs-up'))
       .on('click', function incrementLikes() {
-        console.log('thumbs-up clicked');
-        scope.park.likes++;
+        console.log('thumbs-up clicked for: ', scope.park);
         ParksService.updateLikes(scope.park)
         .then(function showLikedSuccess(parkResponse){
-          console.log('liked park: ', parkResponse);
+          console.log('successfully liked park: ', parkResponse);
         })
         .catch(function showLikedError(err){
           console.error(err);
         });
       });
 
-      $(element.querySelector('.glyphicon-thumbs-down'))
-      .on('click', function incrementDislikes() {
-        console.log('thumbs-down clicked');
-        scope.park.dislikes++;
+      $(element[0].querySelector('.glyphicon-thumbs-down'))
+      .on('click', function incrementLikes() {
+        console.log('thumbs-down clicked for: ', scope.park);
         ParksService.updateDislikes(scope.park)
         .then(function showDislikedSuccess(parkResponse){
-          console.log('disliked park: ', parkResponse);
+          console.log('successfully disliked park: ', parkResponse);
         })
         .catch(function showDislikedError(err){
           console.error(err);
