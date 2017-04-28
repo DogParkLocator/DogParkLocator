966dd2881d4e2b567c675a92a12ea1145809eb6b
Ryan Cronin <Ryan@Ryans-MBP.lan>
2017-04-28 13:38:51 -0400

    error messages for catches added

--- src/frontend/js/park.controller.js
+++ src/frontend/js/park.controller.js
@@ -4,44 +4,65 @@
   angular.module('parks')
   .controller('ParksController', ParksController);
 
-  ParksController.$inject = ['ParksService'];
+  ParksController.$inject = ['$state', '$scope', 'ParksService'];
 
-  function ParksController(ParksService) {
+  function ParksController($state, $scope, ParksService) {
     let vm = this;
     vm.parks = [];
     vm.park = {};
     vm.center = {lat: 38.899, lng: -77.032}; // the iron yard location (for testing only)
+    vm.message = null;
+    vm.hasError = false;
 
+    vm.createPark = function createPark(park) {
+      vm.hasError = false;
+      ParksService.createPark(park)
+        .then(function goToParkList(){
+          $state.go('parks-list');
+        })
+        .catch(function showCreateError(err){
+          vm.message = 'There was a problem creating the new park. Please ensure all fields are correct.';
+          vm.hasError = true;
+
+        });
+    };
+
+    vm.deleteAPark = function deleteAPark(id){
+      vm.hasError = false;
+      console.log('delete a park:', id);
+      return ParksService.deleteAPark(id)
+      .then(function showDeleteSuccess(){
+        vm.message = 'The park has been deleted!';
+        vm.hasError = false;
+        vm.getAllParks()
+          .then(function applyTheScope(data){
+            vm.parks = data;
+          });
+      })
+      .catch(function showDeleteError(err){
+        console.warn(err);
+        vm.message = 'There was a problem deleting the park!.';
+        vm.hasError = true;
+
+      });
+    };
     vm.getAllParks = function getAllParks(){
-      ParksService.getAllParks()
+      vm.hasError = false;
+      return ParksService.getAllParks()
       .then(function addParksToScope(parks) {
+        console.log('add parks to scope:', parks);
         vm.parks = parks;
+        return parks;
       })
       .catch(function handleError(err){
+        vm.hasError = true;
+        vm.message = 'There was a problem getting all the parks!';
         console.error(err);
       });
     };
     vm.getAllParks();
 
-    vm.createPark = function createPark(park) {
-      ParksService.createPark(park);
-      vm.park = {};
-      vm.getAllParks();
-    };
 
-    vm.deleteAPark = function deleteAPark(id){
-      console.log('delete a park:', id);
-
-      return ParksService.deleteAPark(id)
-        .then(function showDeleteSuccess(){
-
-          vm.getAllParks();
-        })
-        .catch(function showDeleteError(err){
-          console.warn(err);
-
-        });
-    };
 
     // function getParkById(id){
     //   if (typeof(id) !== 'string' || id.length === 0) {
