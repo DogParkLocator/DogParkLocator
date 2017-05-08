3edcb6df3d47a22588419edf61613e47db7e7eca
Brian Fiala <bgfiala@gmail.com>
2017-05-08 19:38:19 -0400

    doc blocks added for error handling middleware

--- src/backend/middleware/error-handler.middleware.js
+++ src/backend/middleware/error-handler.middleware.js
@@ -1,3 +1,11 @@
+/**
+ * [errorHandler description]
+ * @param  {Object}   err  the error type and database details
+ * @param  {Object}   req  the request that triggered this function
+ * @param  {Object}   res  the response for the next middleware, or for the frontend
+ * @param  {Function} next [description]
+ * @return {void}
+ */
 module.exports = function errorHandler(err, req, res, next) {
   console.error('ERROR', err.message);
   res.json({message: err.message, time: Date.now(), status: err.status || 500});
